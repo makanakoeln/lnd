e0524cab739990c5974190f65e45bd88b4d63150
makanakoeln <makanakoeln@gmail.com>
2018-07-26 09:59:38 +0200

    added logging

--- scripts/lnd_connect_peers.py
+++ scripts/lnd_connect_peers.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python
-#
-# Script to get a peer list from different url and connect via lncli command
-# just run as cronjob and watch your peer connects grow
-# activate -print- commands for output
 
-import requests, subprocess, json, sys, os
+import requests, subprocess, json, sys, os, logging
+
+log_file="/home/admin/.lnd/connect_peers.log"
+
+logging.basicConfig(filename=log_file,level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
 
 devnull = open(os.devnull, 'wb')
 urls = [ 'https://1ml.com/node?order=capacity&json=true',  
          'https://1ml.com/node?order=lastupdated&json=true', 
          'https://1ml.com/node?order=mostchannels&json=true']
 
-peercommand = "lncli listpeers"
-process     = subprocess.Popen(peercommand.split(), stdout=subprocess.PIPE)
+peerCommand = "lncli listpeers"
+process     = subprocess.Popen(peerCommand.split(), stdout=subprocess.PIPE)
 peers       = json.loads(process.communicate()[0])
 pub_keys    = []
 
-for peer in  peers["peers"]:
+for peer in peers["peers"]:
     pub_keys.append(peer["pub_key"])
 
 ######################
 #parse json from urls
 ######################
+logging.info("---=== Get new peers from 3 url ===---")
 
 for url in urls:
     resp = requests.get(url)
     data = resp.json()
-    #print "Get peers from %s" % url
+    ok_count = 0
+    error_count = 0
+    already_count = 0
     for line in data:
         if len(line["addresses"]) != 0:
             if line["pub_key"] not in pub_keys:
@@ -35,12 +41,20 @@ for url in urls:
                                                                 )
                 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, stderr=devnull)
                 streamdata = process.communicate()[0]
-                #if process.returncode == 0:
-                #    print "%s now connected"  % ( line["pub_key"] )
-                #else:
-                #    print "%s returncode: %d" % ( line["pub_key"], int(process.returncode) )
+                if process.returncode == 0:
+                    #logging.info("%s neu connected"  % ( line["pub_key"] ))
+                    ok_count += 1
+                else:
+                    #logging.info("%s returncode: %d" % ( line["pub_key"], int(process.returncode) ))
+                    error_count += 1
             else:
-                #print "%s already connected"  % ( line["pub_key"] )
+                #logging.info("%s bereits connected"  % ( line["pub_key"] ))
+                already_count += 1
                 continue
         else:
             continue
+logging.info("%s peer(s) already connected" % already_count)
+logging.info("%s peer(s) newly connected" % ok_count)
+logging.info("%s peer(s) with an error" % error_count)
+logging.info("---=== Got all new peers ===---")
+
